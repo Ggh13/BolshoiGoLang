749f66ad8d4a42767e28f2e48b60b510a29f01af
Ggh13 <roma_bzuhor@mail.ru>
2024-10-17 18:28:48 +0300

    HW3 Done

--- cmd/main.go
+++ cmd/main.go
@@ -37,10 +37,9 @@ func main() {
 	fmt.Println(tr)
 	ans, _ := s.LGET("key1", 1)
 	fmt.Println(ans)
-	s.Set("0", "mamba")
-	s.Set("1", "22")
-	s.Set("2", "22.2")
-	fmt.Println(*s.Get("0"), storage.GetType(*s.Get("0")))
-	fmt.Println(*s.Get("1"), storage.GetType(*s.Get("1")))
-	fmt.Println(*s.Get("2"), storage.GetType(*s.Get("2")))
+
+	s.SaveToJson()
+	fmt.Println("__________________")
+	s.ReadJson()
+
 }
--- internal/pkg/storage/storage.go
+++ internal/pkg/storage/storage.go
@@ -1,7 +1,9 @@
 package storage
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
 
 	"go.uber.org/zap"
@@ -13,8 +15,8 @@ type Value struct {
 }
 
 type Storage struct {
-	inner  map[string]Value
-	sql    map[string][]int
+	Inner  map[string]Value `json:"inner"`
+	Sql    map[string][]int `json:"sqlmap"`
 	logger *zap.Logger
 }
 
@@ -28,8 +30,8 @@ func NewStorage() (Storage, error) {
 	logger.Info("created new storage")
 
 	return Storage{
-		inner:  make(map[string]Value),
-		sql:    make(map[string][]int),
+		Inner:  make(map[string]Value),
+		Sql:    make(map[string][]int),
 		logger: logger,
 	}, nil
 }
@@ -44,19 +46,19 @@ func reverseInts(input []int) []int {
 func (r Storage) LPUSH(key string, val []int) (err string) {
 	fmt.Println("Start LLLLPush")
 	for i := range val {
-		_, ok := r.sql[key]
+		_, ok := r.Sql[key]
 		if !ok {
-			r.sql[key] = []int{val[i]}
+			r.Sql[key] = []int{val[i]}
 
 		} else {
-			temp := reverseInts(r.sql[key])
-			r.sql[key] = reverseInts(append(temp, val[i]))
+			temp := reverseInts(r.Sql[key])
+			r.Sql[key] = reverseInts(append(temp, val[i]))
 
 		}
-		fmt.Println(r.sql[key])
+		fmt.Println(r.Sql[key])
 
 	}
-	fmt.Println(r.sql[key])
+	fmt.Println(r.Sql[key])
 	return ""
 
 }
@@ -64,19 +66,19 @@ func (r Storage) LPUSH(key string, val []int) (err string) {
 func (r Storage) RPUSH(key string, val []int) (err string) {
 	fmt.Println("Start RRRRPush")
 	for i := range val {
-		_, ok := r.sql[key]
+		_, ok := r.Sql[key]
 		if !ok {
-			r.sql[key] = []int{val[i]}
+			r.Sql[key] = []int{val[i]}
 
 		} else {
 
-			r.sql[key] = append(r.sql[key], val[i])
+			r.Sql[key] = append(r.Sql[key], val[i])
 
 		}
-		fmt.Println(r.sql[key])
+		fmt.Println(r.Sql[key])
 
 	}
-	fmt.Println(r.sql[key])
+	fmt.Println(r.Sql[key])
 	return ""
 
 }
@@ -87,25 +89,25 @@ func (r Storage) LPOP(key string, val []int) (toPopV []int, err string) {
 
 	if len(val) == 0 {
 
-		toPop = append(toPop, r.sql[key][0])
-		r.sql[key] = r.sql[key][1:]
+		toPop = append(toPop, r.Sql[key][0])
+		r.Sql[key] = r.Sql[key][1:]
 	} else if len(val) == 1 {
-		if val[0] >= len(r.sql[key]) {
+		if val[0] >= len(r.Sql[key]) {
 
-			toPop = append(toPop, r.sql[key]...)
-			r.sql[key] = []int{}
+			toPop = append(toPop, r.Sql[key]...)
+			r.Sql[key] = []int{}
 			return toPop, ""
 		}
 
-		toPop = r.sql[key][:val[0]]
+		toPop = r.Sql[key][:val[0]]
 
-		r.sql[key] = r.sql[key][val[0] : len(r.sql[key])-1]
+		r.Sql[key] = r.Sql[key][val[0] : len(r.Sql[key])-1]
 
 	} else {
 
-		toPop = append(toPop, r.sql[key][val[0]:val[1]+1]...)
+		toPop = append(toPop, r.Sql[key][val[0]:val[1]+1]...)
 
-		r.sql[key] = append(r.sql[key][0:val[0]], r.sql[key][val[1]+1:len(r.sql[key])]...)
+		r.Sql[key] = append(r.Sql[key][0:val[0]], r.Sql[key][val[1]+1:len(r.Sql[key])]...)
 
 	}
 
@@ -116,34 +118,34 @@ func (r Storage) LPOP(key string, val []int) (toPopV []int, err string) {
 func (r Storage) RADDTOSET(key string, val []int) (err string) {
 	fmt.Println("Start RRRRPush")
 	for i := range val {
-		_, ok := r.sql[key]
+		_, ok := r.Sql[key]
 		if !ok {
-			r.sql[key] = []int{val[i]}
+			r.Sql[key] = []int{val[i]}
 
 		} else {
 			flag := false
-			for _, v := range r.sql[key] {
+			for _, v := range r.Sql[key] {
 				if v == val[i] {
 					flag = true
 					break
 				}
 			}
 			if !flag {
-				r.sql[key] = append(r.sql[key], val[i])
+				r.Sql[key] = append(r.Sql[key], val[i])
 			}
 
 		}
 
 	}
-	fmt.Println(r.sql[key])
+	fmt.Println(r.Sql[key])
 	return ""
 }
 func (r Storage) LSET(key string, ind int, elem int) (err string) {
-	_, ok := r.sql[key]
+	_, ok := r.Sql[key]
 	if ok {
 
-		if len(r.sql[key]) > ind {
-			r.sql[key][ind] = elem
+		if len(r.Sql[key]) > ind {
+			r.Sql[key][ind] = elem
 
 			return fmt.Sprintf("(integer) %d", elem)
 		} else {
@@ -154,13 +156,13 @@ func (r Storage) LSET(key string, ind int, elem int) (err string) {
 }
 
 func (r Storage) LGET(key string, ind int) (res int, err string) {
-	_, ok := r.sql[key]
+	_, ok := r.Sql[key]
 	if ok {
 		fmt.Println("r.sql[key]")
-		fmt.Println(len(r.sql[key]))
-		if len(r.sql[key]) > ind {
-			fmt.Println(r.sql[key])
-			return r.sql[key][ind], ""
+		fmt.Println(len(r.Sql[key]))
+		if len(r.Sql[key]) > ind {
+			fmt.Println(r.Sql[key])
+			return r.Sql[key][ind], ""
 		} else {
 			return -1, "Index out of range"
 		}
@@ -168,14 +170,60 @@ func (r Storage) LGET(key string, ind int) (res int, err string) {
 	return -1, "Index out of range1"
 }
 
+func (r Storage) SaveToJson() {
+	jsonData, err := json.Marshal(r)
+	if err != nil {
+		fmt.Println("Ошибка при маршализации:", err)
+		return
+	}
+
+	file, err := os.Create("storage.json")
+	if err != nil {
+		fmt.Println("Ошибка при создании файла:", err)
+		return
+	}
+	defer file.Close() // Закрываем файл после завершения работы
+
+	// Запись JSON в файл
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Println("Ошибка при записи в файл:", err)
+		return
+	}
+
+	fmt.Println(string(jsonData)) // Вывод JSON-строки
+}
+
+func (r Storage) ReadJson() {
+	// Открытие файла
+	file, err := os.Open("storage.json")
+	if err != nil {
+		fmt.Println("Ошибка при открытии файла:", err)
+		return
+	}
+	defer file.Close() // Закрываем файл после завершения работы
+
+	// Чтение содержимого файла
+	decoder := json.NewDecoder(file)
+
+	err = decoder.Decode(&r)
+	if err != nil {
+		fmt.Println("Ошибка при декодировании JSON:", err)
+		return
+	}
+
+	// Вывод прочитанных данных
+	fmt.Println(r)
+}
+
 func (r Storage) Set(key, value string) {
 	switch GetType(value) {
 	case "D":
-		r.inner[key] = Value{s: value, kind: "D"}
+		r.Inner[key] = Value{s: value, kind: "D"}
 	case "Fl64":
-		r.inner[key] = Value{s: value, kind: "Fl64"}
+		r.Inner[key] = Value{s: value, kind: "Fl64"}
 	case "S":
-		r.inner[key] = Value{s: value, kind: "S"}
+		r.Inner[key] = Value{s: value, kind: "S"}
 	}
 
 	r.logger.Info("key set")
@@ -183,7 +231,7 @@ func (r Storage) Set(key, value string) {
 }
 
 func (r Storage) Get(key string) *string {
-	res, ok := r.inner[key]
+	res, ok := r.Inner[key]
 	if !ok {
 		return nil
 	}
